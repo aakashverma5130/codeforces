eb457a896ef512da8860424352188cfdccbfd853
aakashverma5130 <aakashverma5130@gmail.com>
2026-09-14 20:11:17 +0530

    Sync Codeforces: 677A - Vanya and Fence [C++20 (GCC 13-64)]

--- solutions/677A_Vanya_and_Fence.cpp
+++ solutions/677A_Vanya_and_Fence.cpp
@@ -1,15 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        
+    int n,h;
+    cin >> n >> h;
+    int f;
+    int width = 0;
+    for(int i=0;i<n;i++){
+        cin >> f;
+        if(f > h){
+            width += 2;
+        }
+    else if(f <= h){
+        width += 1;
     }
-    return 0;
 }
- 
+cout << width;
+return 0;
+}
